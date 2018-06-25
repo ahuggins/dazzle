bb39e5f402ca54c84c76d1246feb476e87311617
Andrew huggins <andrewhuggins@gmail.com>
2018-06-25 17:46:42 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {    
+    test()
+})
+
+function test() {
+    pct = Math.floor(Math.random()*(360-0+1)+0)
+    bg = "linear-gradient(" + pct + "deg,rgb(0,114,255),rgb(0,183,207),rgb(0,255,160))"
+    $(".bg").css("background-image", bg)
+    setTimeout(() => {
+        test()
+    }, 250);
+}
