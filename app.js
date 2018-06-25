a418d0c52da1064cd991b3d661a3cb2c2d5d4f54
Andrew huggins <andrewhuggins@gmail.com>
2018-06-25 18:11:17 -0400

    use CSS for animating background

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ $(document).ready(function () {
 
 function test() {
     pct = Math.floor(Math.random()*(360-0+1)+0)
+    
     bg = "linear-gradient(" + pct + "deg,rgb(0,114,255),rgb(0,183,207),rgb(0,255,160))"
-    $(".bg").css("background-image", bg)
+    $(".bg").css("background", bg)
     setTimeout(() => {
         test()
     }, 250);
